decad730abd2135b2a066f796fe8c473b3413b82
isarbela <panzeref@gmail.com>
2023-08-24 05:46:09 -0300

    Add dashboard title

--- plantasia/lib/modules/screens/dashboard.dart
+++ plantasia/lib/modules/screens/dashboard.dart
@@ -24,6 +24,7 @@ class _DashboardState extends State<Dashboard> {
       ),
       body: Center(
         child: Container(
+          width: MediaQuery.of(context).size.width,
           margin: const EdgeInsets.only(
             top: 100,
           ),
@@ -32,6 +33,22 @@ class _DashboardState extends State<Dashboard> {
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(200),
               topRight: Radius.circular(200),
+            ),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.only(top: 140.0, right: 16.0, left: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Suas Plantas",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
