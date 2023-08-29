5a12e9b5d05199878f7e9ff825d66ea00aae664f
matteusgui <matteusgui@hotmail.com>
2023-08-29 18:15:31 -0300

    Conclusão da classe Plant Como especificado anteriormente em Kotlin.

--- plantasia/lib/modules/models/plant.dart
+++ plantasia/lib/modules/models/plant.dart
@@ -1,9 +1,26 @@
 class Plant {
   final String name;
+  final String commonName;
+  final List<String> scientificName;
+  final String cycle;
+  final String watering;
+  final String indoor;
+  final String careLevel;
   final int age;
-  final String species;
   final String image;
+  final String description;
 
   Plant(this.image,
-      {required this.name, required this.age, required this.species});
+      {
+        required this.name, 
+        required this.commonName, 
+        required this.scientificName, 
+        required this.cycle, 
+        required this.watering, 
+        required this.indoor, 
+        required this.careLevel, 
+        required this.age, 
+        required this.description
+      }
+  );
 }
