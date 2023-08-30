d1c7494b61c90c5b622aecc26a37a64ea1905a3b
isarbela <panzeref@gmail.com>
2023-08-30 17:02:34 -0300

    Make list item clickable

--- plantasia/lib/widgets/card_item.dart
+++ plantasia/lib/widgets/card_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plantasia/modules/models/plant.dart';
+import 'package:plantasia/modules/screens/plant_details.dart';
 
 class CardItem extends StatelessWidget {
   final Plant plantData;
@@ -8,53 +9,65 @@ class CardItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      color: const Color.fromARGB(255, 65, 100, 74),
-      child: Row(children: [
-        Image.asset(
-          plantData.image,
-          fit: BoxFit.contain,
-          height: 100,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => PlantDetails(
+                    title: 'Plant name',
+                    plantData: plantData,
+                  )),
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
         ),
-        const SizedBox(
-          width: 4,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                plantData.name,
-                style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 242, 227, 219)),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              Text(
-                '${plantData.age} semanas de idade',
-                style: const TextStyle(fontSize: 14, color: Colors.white),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                plantData.commonName.toUpperCase(),
-                style: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 242, 227, 219)),
-              ),
-            ],
+        color: const Color.fromARGB(255, 65, 100, 74),
+        child: Row(children: [
+          Image.asset(
+            plantData.image,
+            fit: BoxFit.contain,
+            height: 100,
           ),
-        )
-      ]),
+          const SizedBox(
+            width: 4,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  plantData.name,
+                  style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 242, 227, 219)),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  '${plantData.age} semanas de idade',
+                  style: const TextStyle(fontSize: 14, color: Colors.white),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  plantData.commonName.toUpperCase(),
+                  style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 242, 227, 219)),
+                ),
+              ],
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
